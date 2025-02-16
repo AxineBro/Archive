bc6651e7b92db3ab4e5ba0067f02262d3d91537f
Axine <axine.bro009@gmail.com>
2025-02-16 14:36:34 +0300

    public Task Archive 1S1C

---
+++ Exam1Semester1Сourse/Exam.v2/Exam.v2/src/ru/vsu/cs/course1/Exam.java
@@ -0,0 +1,44 @@
+package ru.vsu.cs.course1;
+
+import java.util.*;
+
+/**
+ * Класс для реализации задач
+ */
+public class Exam {
+
+    public static void task1(int n) {
+       return;
+    }
+
+    public static void task2(int n) {
+        return;
+    }
+
+    public static void task3(int h) {
+        return;
+    }
+
+    public static void task4(int h, int w) {
+
+        return;
+    }
+
+    public static int task5(int[] arr) {
+        return -1;
+    }
+
+    public static int task6(int[][] arr) {
+        return -1;
+    }
+
+    public static void task7(int[][] arr) {
+        return;
+    }
+    public static void task8(int[][] arr) {
+        return;
+    }
+    public static List<Integer> task9(List<Integer> list) {
+       return null;
+    }
+}
