a9048c8deb551aba0493b4b4821ef91b4fb6f2ac
Axine <axine.bro009@gmail.com>
2025-02-16 16:44:12 +0300

    edit dir Exam

--- Exam1Semester1Сourse(ВВП)/Exam.v2/Exam.v2/src/ru/vsu/cs/course1/Exam.java
+++ Exam1Semester1Сourse(ВВП)/Exam.v2/Exam.v2/src/ru/vsu/cs/course1/Exam.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Exam {
 
     public static void task1(int n) {
-    	return;
+        return;
     }
 
     public static void task2(int n) {
@@ -20,7 +20,6 @@ public class Exam {
     }
 
     public static void task4(int h, int w) {
-
         return;
     }
 
@@ -28,7 +27,31 @@ public class Exam {
         return -1;
     }
 
+    private static boolean indexOf(int indexString, int num, int[][] arrs){
+        for(int i = 0; i < arrs.length; i++){
+            if(i == indexString) continue;
+            for (int j = 0; j < arrs[i].length; j++){
+                if(arrs[i][j] == num){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private static boolean isUnique(int indexString, int[][] arrs){
+        for(int i = 0; i < arrs[indexString].length; i++) {
+            if(!indexOf(indexString, arrs[indexString][i], arrs)){
+                return false;
+            }
+        }
+        return true;
+    }
     public static int task6(int[][] arr) {
+        for(int i = 0; i < arr.length; i++){
+            if(isUnique(i, arr)){
+                return i;
+            }
+        }
         return -1;
     }
 
@@ -36,26 +59,45 @@ public class Exam {
         return;
     }
     public static void task8(int[][] arr) {
+        int counter = 0;
+        int counterCount = 0;
+        for (int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if((arr.length - 1 -i)%2 != 0) {
+                    arr[arr.length - 1 - i][j] = counter;
+                    counterCount++;
+                    if (counterCount >= 2) {
+                        counterCount = 0;
+                        counter++;
+                    }
+                }
+                else {
+                    arr[arr.length - 1 - i][arr[i].length-1-j] = counter;
+                    counterCount++;
+                    if (counterCount >= 2) {
+                        counterCount = 0;
+                        counter++;
+                    }
+                }
+            }
+        }
         return;
     }
     public static List<Integer> task9(List<Integer> list) {
-    	boolean previousElementIsPositive = true;
-    	boolean currentElementIsPositive = true;
-    	List<Integer> resultList = new ArrayList<Integer>();
-    	for(int num : list) {
-    		if(num < 0 && currentElementIsPositive) {
-    			currentElementIsPositive = false;
-    		} else if(num < 0 && previousElementIsPositive) {
-    			previousElementIsPositive = false;
-    		} else if(!currentElementIsPositive && !previousElementIsPositive) {
-    			resultList.add(num);
-    		}
-    		if(num >= 0) {
-    			previousElementIsPositive = true;
-    	    	currentElementIsPositive = true;
-    		}
-    		resultList.add(num);
-    	}
-    	return resultList;
+        List<Integer> resultList = new ArrayList<>();
+        int dataValue = 0;
+        boolean isStart = false;
+        for(int value : list){
+            if(isStart){
+                if(value > 0 && dataValue > 0){
+                    resultList.add(value+dataValue);
+                }
+            }
+            else{
+                isStart = true;
+            }
+            dataValue = value;
+        }
+        return resultList;
     }
 }
